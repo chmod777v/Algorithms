f8165c3eb3981843c5298db957a94083251dccc6
chmod777v <ivan.pylaev1@gmail.com>
2025-11-29 20:41:44 +0300

    7)Task pool Update: Create 3)Live coding/fetchParallel

--- 7)Task pool/3)Live coding/fetchParallel/solution/solution.go
+++ 7)Task pool/3)Live coding/fetchParallel/solution/solution.go
@@ -3,43 +3,64 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
 // Написать функцию, которая запрашивает URL из списка и в случае положительного кода 200 выводит
 // в stdout в отдельной строке url: , code:
 // В случае ошибки выводит в отдельной строке url: , code:
+// Вывод должен быть синхронный(в каком порядке горутины выводят в таком он и должен выводится)
 // Функция должна завершаться при отмене контекста.
-// Доп. задание: реализовать ограничение количества одновременно запущенных горутин.
+// Реализовать ограничение количества одновременно запущенных горутин.
 
 func fetchParallel(ctx context.Context, urls []string) {
+	const concurrentLimit = 3
+	sem := make(chan struct{}, concurrentLimit)
 	httpClient := &http.Client{}
+	wg := sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
 	for _, url := range urls {
-		req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-		resp, _ := httpClient.Do(req)
-		fmt.Println(resp.Body)
+		select {
+		case <-ctx.Done():
+			return
+		case sem <- struct{}{}:
+		}
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			defer func() { <-sem }()
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+			if err != nil {
+				fmt.Printf("err:%v\n", err.Error())
+				return
+			}
+			resp, err := httpClient.Do(req)
+			if err != nil {
+				fmt.Printf("err:%v\n", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			mu.Lock()
+			fmt.Printf("url: %s		code: %v\n", url, resp.Status)
+			mu.Unlock()
+		}(url)
 	}
+	wg.Wait()
 }
 
 func main() {
 	urls := []string{
 		"https://www.yandex.ru",
-		"https://www.maill.ru",
+		"https://www.mail.ru",
 		"https://www.google.com",
 		"https://mail.google.com",
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	httpClient := &http.Client{}
-	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.google.com", nil)
-	resp, _ := httpClient.Do(req)
-	defer resp.Body.Close()
-	val, _ := io.ReadAll(resp.Body)
-	fmt.Printf("%s", val)
-
-	//fetchParallel(ctx, urls)
-
+	fetchParallel(ctx, urls)
 }
